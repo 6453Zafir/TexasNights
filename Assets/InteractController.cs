a38b00cc775e99d49330469945894407bd718ade
6453Zafir <511599737@qq.com>
2016-10-20 14:29:04 +0800

    animate camera unfinished

---
+++ Assets/InteractController.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	public void hideAllButton(){
+		foreach (Transform child in transform)     
+		{  
+			child.gameObject.SetActiveRecursively(false);   
+		}   
+	}
+	public void showAllButton(){
+		foreach (Transform child in transform)     
+		{  
+			child.gameObject.SetActiveRecursively(true);   
+		}   
+	}
+}
