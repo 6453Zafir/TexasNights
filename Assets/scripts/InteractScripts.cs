a6363b8dff4b80ecfadf7bcd7200a48baa33fbda
6453Zafir <511599737@qq.com>
2016-10-20 14:24:30 +0800

    1...

---
+++ Assets/scripts/InteractScripts.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractScripts : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	public void HideAllButton()  
+	{    
+		foreach (Transform child in transform)     
+		{  
+			child.gameObject.SetActiveRecursively(false);   
+		}   
+	}
+
+	public void ShowAllButton()  
+	{    
+		foreach (Transform child in transform)     
+		{  
+			child.gameObject.SetActiveRecursively(true);   
+		}   
+	}
+}
