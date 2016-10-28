3cfdb05ca0316989178e290c1e73389f497806f8
6453Zafir <511599737@qq.com>
2016-10-28 16:01:54 +0800

    prefab added, made the play background more compatible,and add switch between scenes,but still a little problem :|

---
+++ Assets/scripts/HelpViewController.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+
+public class HelpViewController : MonoBehaviour {
+
+	public GameObject helpPanel;
+	public GameObject RulePanel;
+	public Button leaveButton;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (helpPanel != null) {
+			HideAlertIfClickedOutside (helpPanel);
+		}
+		if (RulePanel != null) {
+			HideAlertIfClickedOutside (RulePanel);
+		}
+
+
+	}
+	private void HideAlertIfClickedOutside(GameObject panel) {
+		if (Input.GetMouseButton(0) && panel.activeSelf && 
+		    !RectTransformUtility.RectangleContainsScreenPoint(
+			panel.GetComponent<RectTransform>(), 
+			Input.mousePosition, 
+			Camera.main)) {
+			panel.SetActive(false);
+			GameObject panelParent = panel.transform.parent.gameObject;
+			panelParent.SetActive(false);
+		}
+	}
+
+	public void backToMain(){
+		Application.LoadLevel ("menu");
+		Debug.Log ("level loaded");
+	}
+
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public Vector3 location = new Vector3(0,0,0); // place you want it
+	// Use this for initialization
+	void Start () {	
+		Transform Playerparent = GameObject.Find("Players").transform;
+		GameObject player1 = Instantiate(Resources.Load("Player")) as GameObject;  // instatiate the object
+		player1.transform.parent = Playerparent;
+		player1.transform.position = new Vector3 (-620,220, 0);
+		player1.transform.localScale = new Vector3(1, 1, 1);
+
+		GameObject player2 = Instantiate(Resources.Load("Player"), location, Quaternion.identity) as GameObject;  // instatiate the object
+		player2.transform.parent = Playerparent;
+		player2.transform.position = new Vector3 (-470,-70, 0);
+		player2.transform.localScale = new Vector3(1, 1, 1);
+
+		GameObject player3 = Instantiate(Resources.Load("Player"), location, Quaternion.identity) as GameObject;  // instatiate the object
+		player3.transform.parent = Playerparent;
+		player3.transform.position = new Vector3 (-180,-250, 0);
+		player3.transform.localScale = new Vector3(1, 1, 1);
+
+		GameObject player4 = Instantiate(Resources.Load("Player"), location, Quaternion.identity) as GameObject;  // instatiate the object
+		player4.transform.parent = Playerparent;
+		player4.transform.position = new Vector3 (180,-250, 0);
+		player4.transform.localScale = new Vector3(1, 1, 1);
+
+		GameObject player5 = Instantiate(Resources.Load("Player"), location, Quaternion.identity) as GameObject;  // instatiate the object
+		player5.transform.parent = Playerparent;
+		player5.transform.position = new Vector3 (470,-70, 0);
+		player5.transform.localScale = new Vector3(1, 1, 1);
+
+		GameObject player6 = Instantiate(Resources.Load("Player"), location, Quaternion.identity) as GameObject;  // instatiate the object
+		player6.transform.parent = Playerparent;
+		player6.transform.position = new Vector3 (620,220, 0);
+		player6.transform.localScale = new Vector3(1, 1, 1);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
