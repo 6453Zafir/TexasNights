59c681f91c1fdc426039c15aaed7fac70d3df176
6453Zafir <511599737@qq.com>
2016-11-01 16:00:38 +0800

    player position adjusted

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -2,38 +2,51 @@
 using System.Collections;
 
 public class PlayerController : MonoBehaviour {
-	public Vector3 location = new Vector3(0,0,0); // place you want it
+	public GameObject Player1Labell;
+	public GameObject Player1Label2;
+	public GameObject Player1Label3;
+	public GameObject Player1Label4;
+	public GameObject Player1Label5;
+	public GameObject Player1Label6;
+	
 	// Use this for initialization
 	void Start () {	
-		Transform Playerparent = GameObject.Find("Players").transform;
+		//Transform Playerparent = GameObject.Find("Players").transform;
+		Transform Playerl1 = Player1Labell.transform;
+		Transform Player12 = Player1Label2.transform;
+		Transform Player13 = Player1Label3.transform;
+		Transform Player14 = Player1Label4.transform;
+		Transform Player15 = Player1Label5.transform;
+		Transform Player16 = Player1Label6.transform;
+
 		GameObject player1 = Instantiate(Resources.Load("Player")) as GameObject;  // instatiate the object
-		player1.transform.parent = Playerparent;
-		player1.transform.position = new Vector3 (-620,200, 0);
+		player1.transform.parent = Playerl1;
+		player1.transform.localPosition = new Vector3 (-20, 0, 0);
 		player1.transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
 
-		GameObject player2 = Instantiate(Resources.Load("Player"), location, Quaternion.identity) as GameObject;  // instatiate the object
-		player2.transform.parent = Playerparent;
-		player2.transform.position = new Vector3 (-470,-90, 0);
+		GameObject player2 = Instantiate(Resources.Load("Player")) as GameObject;  // instatiate the object
+		player2.transform.parent = Player12;
+		player2.transform.localPosition = new Vector3 (-20, 0, 0);
 		player2.transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
 
-		GameObject player3 = Instantiate(Resources.Load("Player"), location, Quaternion.identity) as GameObject;  // instatiate the object
-		player3.transform.parent = Playerparent;
-		player3.transform.position = new Vector3 (-180,-270, 0);
+		GameObject player3 = Instantiate(Resources.Load("Player")) as GameObject;  // instatiate the object
+		player3.transform.parent = Player13;
+		player3.transform.localPosition = new Vector3 (-20, 0, 0);
 		player3.transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
 
-		GameObject player4 = Instantiate(Resources.Load("Player"), location, Quaternion.identity) as GameObject;  // instatiate the object
-		player4.transform.parent = Playerparent;
-		player4.transform.position = new Vector3 (180,-270, 0);
+		GameObject player4 = Instantiate(Resources.Load("Player")) as GameObject;  // instatiate the object
+		player4.transform.parent = Player14;
+		player4.transform.localPosition = new Vector3 (-20, 0, 0);
 		player4.transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
 
-		GameObject player5 = Instantiate(Resources.Load("Player"), location, Quaternion.identity) as GameObject;  // instatiate the object
-		player5.transform.parent = Playerparent;
-		player5.transform.position = new Vector3 (470,-90, 0);
+		GameObject player5 = Instantiate(Resources.Load("Player")) as GameObject;  // instatiate the object
+		player5.transform.parent = Player15;
+		player5.transform.localPosition = new Vector3 (-20, 0, 0);
 		player5.transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
 
-		GameObject player6 = Instantiate(Resources.Load("Player"), location, Quaternion.identity) as GameObject;  // instatiate the object
-		player6.transform.parent = Playerparent;
-		player6.transform.position = new Vector3 (620,200, 0);
+		GameObject player6 = Instantiate(Resources.Load("Player")) as GameObject;  // instatiate the object
+		player6.transform.parent = Player16;
+		player6.transform.localPosition = new Vector3 (-20, 0, 0);
 		player6.transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
 
 	}
