d69eb51ea94aad10478e3e66e89bc8ae5ae7ee82
6453Zafir <511599737@qq.com>
2016-11-02 10:06:10 +0800

    player prefab fixed

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -18,36 +18,41 @@ public class PlayerController : MonoBehaviour {
 		Transform Player14 = Player1Label4.transform;
 		Transform Player15 = Player1Label5.transform;
 		Transform Player16 = Player1Label6.transform;
-		
+		/*
+		GameObject player = Instantiate(Resources.Load("Player")) as GameObject;  // instatiate the object
+		player.transform.SetParent (Playerl1, false);
+		player.transform.position = new Vector3 (-20, 0, 0);
+		player.transform.localScale = new Vector3(1f, 1f, 1f);
+		*/
 		GameObject player1 = Instantiate(Resources.Load("Player")) as GameObject;  // instatiate the object
 		player1.transform.parent = Playerl1;
 		player1.transform.localPosition = new Vector3 (-20, 0, 0);
-		player1.transform.localScale = new Vector3(0.01f, 0.01f, 0.01f);
+		player1.transform.localScale = new Vector3(1f, 1f, 1f);
 		
 		GameObject player2 = Instantiate(Resources.Load("Player")) as GameObject;  // instatiate the object
 		player2.transform.parent = Player12;
 		player2.transform.localPosition = new Vector3 (-20, 0, 0);
-		player2.transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
+		player2.transform.localScale = new Vector3(1f, 1f, 1f);
 		
 		GameObject player3 = Instantiate(Resources.Load("Player")) as GameObject;  // instatiate the object
 		player3.transform.parent = Player13;
 		player3.transform.localPosition = new Vector3 (-20, 0, 0);
-		player3.transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
+		player3.transform.localScale = new Vector3(1f, 1f, 1f);
 		
 		GameObject player4 = Instantiate(Resources.Load("Player")) as GameObject;  // instatiate the object
 		player4.transform.parent = Player14;
 		player4.transform.localPosition = new Vector3 (-20, 0, 0);
-		player4.transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
+		player4.transform.localScale = new Vector3(1f, 1f, 1f);
 		
 		GameObject player5 = Instantiate(Resources.Load("Player")) as GameObject;  // instatiate the object
 		player5.transform.parent = Player15;
 		player5.transform.localPosition = new Vector3 (-20, 0, 0);
-		player5.transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
+		player5.transform.localScale = new Vector3(1f, 1f, 1f);
 		
 		GameObject player6 = Instantiate(Resources.Load("Player")) as GameObject;  // instatiate the object
 		player6.transform.parent = Player16;
 		player6.transform.localPosition = new Vector3 (-20, 0, 0);
-		player6.transform.localScale = new Vector3(0.35f, 0.35f, 0.35f);
+		player6.transform.localScale = new Vector3(1f, 1f, 1f);
 		
 	}
 	
