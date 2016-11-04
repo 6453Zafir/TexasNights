7877c08103a960bce39c6adb28cf606e48c7af20
6453Zafir <511599737@qq.com>
2016-11-04 18:12:08 +0800

    end of this week

---
+++ Assets/scripts/TableCardController.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TableCardController : MonoBehaviour {
+	private bool firstThree = false;
+	private bool the4th = false;
+	private bool the5th = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	 
+	}
+}
