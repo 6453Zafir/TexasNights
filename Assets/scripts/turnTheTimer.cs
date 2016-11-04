644c6fc2c6e45adb807762d36dd43d708f9ac33e
6453Zafir <511599737@qq.com>
2016-11-04 11:19:31 +0800

    time turnner function

---
+++ Assets/scripts/turnTheTimer.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class turnTheTimer : MonoBehaviour {
+	Vector3 spawnPosition;
+	Vector3 spawnRotaion;
+	Vector2 anchor;
+	// Use this for initialization
+	void Start () {
+		spawnPosition = new Vector3(0f,408f,0); //if you want to always spwan in the middle
+		anchor = new Vector2 (0f, 410.2f);
+		InvokeRepeating("ChangePosition", 0, 1); 
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	private void ChangePosition(){
+		transform.position = spawnPosition;
+		Debug.Log (transform.eulerAngles.z);
+		if (transform.eulerAngles.z > 60 &&transform.eulerAngles.z <210) {
+			transform.Rotate(0,0,210);
+		}else{
+			transform.Rotate (0, 0, 30);
+		}
+
+		//spawnPosition = new Vector3 (Random.Range(-5,5), Random.Range(-5,5), Random.Range(-5,5));
+	}
+}
