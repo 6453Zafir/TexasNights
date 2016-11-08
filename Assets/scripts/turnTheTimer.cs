e9967bb4def5120e9ed173d126501be252982410
6453Zafir <511599737@qq.com>
2016-11-08 10:50:26 +0800

    two timer scripts merged

--- Assets/scripts/turnTheTimer.cs
+++ Assets/scripts/turnTheTimer.cs
@@ -4,66 +4,89 @@ using UnityEngine.UI;
 
 public class turnTheTimer : MonoBehaviour {
 	public GameObject theTurningBar;
-	public GameObject TimerText;
+	public Button addtimeButton;
+	public Text timerText;
 	public Image cooldownMask;
-	public float waitTime = 10.0f;
+	public float timeLimit = 10;
 	public int currentPlayer;
+
+	public bool isTimeAdded = false;
+	public bool isTimerSwitched = false;
+
 	Vector3 spawnPosition;
 	Vector3 spawnRotaion;
-	private bool isTimerJumped = false;
 	// Use this for initialization
 	void Start () {
-		spawnPosition = new Vector3(0f,408f,0); //if you want to always spwan in the middle
+		currentPlayer = 0;  
+		spawnPosition = new Vector3(0f,408f,0);
 		InvokeRepeating("ChangePosition", 0, 10); 
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		currentPlayer = TimerText.GetComponent<Timer> ().currentPlayer;
-		isTimerJumped = TimerText.GetComponent<Timer> ().isTimerSwitched;
+
+		if (timeLimit >= 0) {
+			timeLimit -= Time.deltaTime;
+			isTimerSwitched = false;
+			timerText.text = timeLimit.ToString ("f1");
+			if(timeLimit < 5){
+				timerText.color = new Color(0.882f,0.447f,0.365f);
+			}
+		} else {
+			Debug.Log("Time is over");
+			if(currentPlayer<5){
+				currentPlayer++;
+			}else{
+				currentPlayer = 0;
+			}
+			timerText.color = new Color(1,1,1);
+			timeLimit = 10;
+			addtimeButton.interactable = true;
+			isTimeAdded = false;
+			isTimerSwitched = true;
+		}
 		print ("currentPalyer"+currentPlayer);
-		print ("isswitching "+isTimerJumped);
+		print ("isswitching "+isTimerSwitched);
 		switch (currentPlayer) {
 		case 0:
-			if(isTimerJumped){
+			if(isTimerSwitched){
 				cooldownMask.fillAmount = 0.832f;
-				isTimerJumped = false;
+				//isTimerSwitched = false;
 			}
 			cooldownMask.fillAmount -= 1.0f/100 * Time.deltaTime;
 			break;
 		case 1:		
-			if(isTimerJumped){
+			if(isTimerSwitched){
 				cooldownMask.fillAmount = 0.915f;
-				isTimerJumped = false;
+				isTimerSwitched = false;
 			}
 			cooldownMask.fillAmount -= 1.0f/110* Time.deltaTime;
 			break;
 		case 2:
-			if(isTimerJumped){
+			if(isTimerSwitched){
 				cooldownMask.fillAmount = 0.999f;
-				isTimerJumped = false;
+				isTimerSwitched = false;
 			}
 			cooldownMask.fillAmount -= 1.0f/120 * Time.deltaTime;
 			break;
 		case 3:
-			if(isTimerJumped){
+			if(isTimerSwitched){
 				cooldownMask.fillAmount = 0.087f;
-				isTimerJumped = false;
+				isTimerSwitched = false;
 			}
 			cooldownMask.fillAmount -= 1.0f/110 * Time.deltaTime;
 			break;
 		case 4:
-			if(isTimerJumped){
+			if(isTimerSwitched){
 				cooldownMask.fillAmount = 0.167f;
-				isTimerJumped = false;
+				isTimerSwitched = false;
 			}
 			cooldownMask.fillAmount -= 1.0f/120 * Time.deltaTime;
 			break;
 		case 5:
-			if(isTimerJumped){
+			if(isTimerSwitched){
 				cooldownMask.fillAmount = 0.251f;
-				isTimerJumped = false;
+				isTimerSwitched = false;
 			}
 			cooldownMask.fillAmount -= 1.0f/130 * Time.deltaTime;
 			break;
@@ -81,6 +104,18 @@ public class turnTheTimer : MonoBehaviour {
 		}else{
 			print(theTurningBar.transform.eulerAngles.z);
 			theTurningBar.transform.Rotate (0, 0, 30);
+		}
+	}
+
+	public void addTime(){
+		if (isTimeAdded == false) {
+			timeLimit += 5;
+			isTimeAdded = true;
+			addtimeButton.interactable = false;
+			timerText.color = new Color(0.906f,0.804f,0.125f);
+			timerText.fontSize = 90;
+		} else {
+			Debug.Log("there is only one chance to add time");
 		}
 	}
 }
