7d04c6320bf23ede7d8116571093b038b44977c0
6453Zafir <511599737@qq.com>
2016-11-21 14:32:24 +0800

    json test

---
+++ Assets/scripts/UserEntity.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+
+public class User
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public string token { get; set; }
+	/// <summary>
+	/// 
+	/// </summary>
+	public string phoneNum { get; set; }
+	/// <summary>
+	/// 
+	/// </summary>
+	public string password { get; set; }
+	/// <summary>
+	/// 
+	/// </summary>
+	public string nickname { get; set; }
+	/// <summary>
+	/// 
+	/// </summary>
+	public string gender { get; set; }
+	/// <summary>
+	/// 
+	/// </summary>
+	public int score { get; set; }
+	/// <summary>
+	/// 
+	/// </summary>
+	public int range { get; set; }
+	/// <summary>
+	/// 
+	/// </summary>
+	public int avatar { get; set; }
+	/// <summary>
+	/// 
+	/// </summary>
+	public int level { get; set; }
+	/// <summary>
+	/// 
+	/// </summary>
+	public double winRate { get; set; }
+	/// <summary>
+	/// 
+	/// </summary>
+	public double inRace { get; set; }
+	/// <summary>
+	/// 
+	/// </summary>
+	public int playtimes { get; set; }
+	/// <summary>
+	/// 
+	/// </summary>
+	public int maxScore { get; set; }
+}
+
+public class Root
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	public List <User> user { get; set; }
+}
---
+++ Assets/scripts/parseJson.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using LitJson;
+
+public class parseJson : MonoBehaviour {
+	private string jsonString;
+	private JsonData itemData;
+
+	// Use this for initialization
+	void Start () {
+		jsonString = File.ReadAllText (Application.dataPath + "/scripts/user.json");
+		itemData = JsonMapper.ToObject (jsonString);
+		//Debug.Log (itemData["user"][0]["phoneNum"]);
+		Debug.Log(GetItem(1,"room")["currentPlayer"]);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	JsonData GetItem(int roomType,string type){
+		Debug.Log ("length"+itemData[type].Count);
+		for (int i = 0; i<itemData[type].Count; i++) {
+			if(itemData[type][i]["roomtype"].Equals(roomType)){
+				return itemData[type][i];
+			}
+		}
+		return null;
+	}
+}
